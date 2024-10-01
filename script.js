900d6e53320d6df341e25db69657f20d58474831
Kwibs <kylehelstowski@gmail.com>
2024-10-01 11:51:14 -0700

    change

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 window.addEventListener('scroll', function() {
-    let scrollPos = window.scrollY;
-    let windowHeight = document.body.scrollHeight - window.innerHeight;
-    let opacityFactor = 1 - (scrollPos / (windowHeight / 1.2));
-    
-    document.body.style.opacity = Math.max(opacityFactor, 0); 
+  let scrollPos = window.scrollY;
+  let windowHeight = document.body.scrollHeight - window.innerHeight;
+  let opacityFactor = 1 - (scrollPos / (windowHeight / 1.2));
+  
+ 
+  let contentBlocks = document.querySelectorAll('.image-block'); 
+  contentBlocks.forEach(block => {
+      block.style.opacity = Math.max(opacityFactor, 0); 
   });
+
   
-  
-  document.getElementById('resetButton').addEventListener('click', function() {
-    window.scrollTo({
+  document.getElementById('resetButton').style.opacity = '1'; 
+});
+
+document.getElementById('resetButton').addEventListener('click', function() {
+  window.scrollTo({
       top: 0,
       behavior: 'smooth'
-    });
-    document.body.style.opacity = 10; 
   });
   
+  
+  let contentBlocks = document.querySelectorAll('.image-block');
+  contentBlocks.forEach(block => {
+      block.style.opacity = '1'; 
+  });
+});
